1f66ad4c1e5c072661d569994b7d2e284ec52e77
Konstantin <79932896+kostan87@users.noreply.github.com>
2021-07-29 12:13:54 +0500

    Add files

---
+++ store/store.php
@@ -0,0 +1,34 @@
+<main>
+  <div class="feed">
+    <div class="place"><div id="place1"><a href="">
+      <img src="http://store/img/id1.svg" alt="">
+      <p>Футбольный мяч</p>
+      <span> 100 RUB</span>
+    </a></div></div>
+    <div class="place"><div id="place2"><a href="">
+      <img src="http://store/img/id2.svg" alt="">
+      <p>Смартфон</p>
+      <span>2000 RUB</span>
+    </a></div></div>
+        <div class="place"><div id="place3"><a href="">
+          <img src="http://store/img/id3.svg" alt="">
+          <p>Геймпад</p>
+          <span>1000 RUB</span>
+        </a></div></div>
+        <div class="place"><div id="place4"><a href="">
+          <img src="http://store/img/id4.svg" alt="">
+          <p>Диван</p>
+          <span>2500 RUB</span>
+        </a></div></div>
+        <div class="place"><div id="place5"><a href="">
+          <img src="http://store/img/id5.svg" alt="">
+          <p>Наушники</p>
+          <span>800 RUB</span>
+        </a></div></div>
+        <div class="place"><div id="place6"><a href="">
+          <img src="http://store/img/id6.svg" alt="">
+          <p>Камера</p>
+          <span>1200 RUB</span>
+        </a></div></div>
+  </div>
+</main>
